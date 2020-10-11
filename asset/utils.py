905adf1f4ae26fe527d2fe83c6be39e49a3dba01
Hafez Farazi <hfarazi@users.noreply.github.com>
2020-10-12 01:28:18 +0200

    Update utils.py

--- asset/utils.py
+++ asset/utils.py
@@ -453,6 +453,18 @@ def complex_abs(iT):
     outR = torch.sqrt(iTre**2 + iTim**2+ 1e-8)
     return torch.cat([outR, torch.zeros_like(outR)], -1)
 
+def complex_division(t1, t2, eps=1e-8):
+    assert t1.size() == t2.size()
+    assert t1.size()[-1] == 2
+    assert t1.device==t2.device
+    t1re = torch.index_select(t1, -1, torch.tensor([0], device=t1.device))
+    t1im = torch.index_select(t1, -1, torch.tensor([1], device=t1.device))
+    t2re = torch.index_select(t2, -1, torch.tensor([0], device=t1.device))
+    t2im = torch.index_select(t2, -1, torch.tensor([1], device=t1.device))
+    denominator = t2re**2 + t2im**2 + eps
+    numeratorRe = t1re*t2re + t1im*t2im
+    numeratorIm = t1im*t2re - t1re*t2im
+    return torch.cat([numeratorRe/denominator, numeratorIm/denominator], -1)
 
 def listToTensor(inp):
     return torch.stack(inp,dim=1).unsqueeze(1)
